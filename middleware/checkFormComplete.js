0e05d606865737c12480fef17c0bc3b37b5b21d7
= <=>
2020-06-30 19:21:58 -0400

    Fix regex

--- middleware/checkFormComplete.js
+++ middleware/checkFormComplete.js
@@ -1,22 +1,15 @@
 /* 
     ENDPOINT(S): POST /signup
 
-    PURPOSE: This middleware function checks to make sure the user completely filled out
-             the signup form and that it passes a basic REGEX test.
-             For now, the requirements for a username and password are simple: A username cant be empty
-             and a password must be at least 8 characters. The REGEX for these are provided in this file.
+    PURPOSE: This middleware function checks to make sure the user completely 
+    filled out the signup form and that it passes a REGEX test.
+    
 */
 
 function checkFormComplete(req, res, next) {
   const { username, password } = req.body;
 
   const usernameRegex = /^[a-z0-9]{5,31}$/i;
-
-  /*
-    Got this password regex from stackoverflow. I gotta give credit where its due.
-
-    https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-*/
 
   // Minimum eight characters, at least one letter and one number:
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
